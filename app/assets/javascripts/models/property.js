becde176057aa21d2b1425c71fdd831e6141300b
Sai To Yeung <saitoyeung@gmail.com>
2014-09-02 14:13:02 -0400

    Adds edits to current user implementation

--- app/assets/javascripts/models/property.js
+++ app/assets/javascripts/models/property.js
@@ -1,3 +1,11 @@
 StreetEasyClone.Models.Property = Backbone.Model.extend({
 	urlRoot: "/api/properties",
+	
+	parse: function(response) {
+		if (response.current_user) {
+			StreetEasyClone.currentUser = response.current_user;
+			delete response.current_user
+		}
+		return response;
+	}
 });
