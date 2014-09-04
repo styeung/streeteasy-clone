f9b4ee40652af1a708685babf4fafd8b41f5c020
Sai To Yeung <saitoyeung@gmail.com>
2014-09-04 11:34:23 -0400

    Adds photo view with thumbnails

---
+++ app/assets/javascripts/views/album_photos_view.js
@@ -0,0 +1,24 @@
+StreetEasyClone.Views.AlbumPhotosView = Backbone.View.extend({
+	template: JST["templates/album_photos_view"],
+	
+	initialize: function (options) {
+		this.main_photo_url = options.main_photo_url;
+	},
+	
+	events: {
+		"click .thumbnail": "switchPhoto"
+	},
+	
+	render: function (options) {
+		var content = this.template({album_photos: this.collection, main_photo_url: this.main_photo_url});
+		this.$el.html(content);
+		return this;
+	},
+	
+	switchPhoto: function(event) {
+		console.log("here");
+		$(".thumbnail-container li").removeClass("active")
+		$(event.currentTarget).parent().toggleClass("active");
+		$(".displayed_image").attr("src", $(event.currentTarget).attr("src"));
+	}
+})
--- app/assets/javascripts/views/property_show.js
+++ app/assets/javascripts/views/property_show.js
@@ -6,9 +6,12 @@ StreetEasyClone.Views.PropertyShow = Backbone.View.extend({
 		var content = this.template({property: property});
 		
 		this.$el.html(content);
+		
+		var albumPhotosView = new StreetEasyClone.Views.AlbumPhotosView({ collection: property.album_photos(), main_photo_url: property.escape("property_photo_url") });
+		this.$(".photos-container").html(albumPhotosView.render().$el);
 
-		var commentsListSubView = new StreetEasyClone.Views.CommentsList({ collection: property.comments() });
-		this.$(".comments-list-container").html(commentsListSubView.render().$el);
+		var commentsListView = new StreetEasyClone.Views.CommentsList({ collection: property.comments() });
+		this.$(".comments-list-container").html(commentsListView.render().$el);
 		
 		return this;
 	}
